a3bf7d7726693eb7a33b458445de71680a64814f
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2020-10-06 12:02:01 +0500

    Создал прототип конструктора сайта

---
+++ src/classes/sidebar.js
@@ -0,0 +1,37 @@
+import {block} from "../utils";
+import {TextBlock, TitleBlock} from "./block";
+
+export class Sidebar {
+    constructor(selector, updateCallback) {
+        this.$el = document.querySelector(selector);
+        this.update = updateCallback;
+        this.init();
+    }
+
+    init() {
+        this.$el.insertAdjacentHTML('afterbegin', this.template);
+        this.$el.addEventListener('submit', this.add.bind(this))
+    }
+
+    get template() {
+        return [
+            block('text'),
+            block('title')
+        ].join('')
+    }
+
+    add(evt) {
+        evt.preventDefault();
+        const type = evt.target.name;
+        const value = evt.target.value.value;
+        const styles = evt.target.styles.value;
+
+
+        const newBlock = type === 'text'
+            ? new TextBlock(value, {styles})
+            : new TitleBlock(value, {styles});
+        this.update(newBlock);
+        evt.target.reset();
+    }
+}
+
